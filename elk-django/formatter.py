fc04b556956c6b037ab9161c57454a0d876fe748
neonua666 <igor.g@enkonix.com>
2021-04-29 13:34:41 +0300

    add more data for analize user and ip

--- elk-django/formatter.py
+++ elk-django/formatter.py
@@ -4,8 +4,21 @@ from logstash.formatter import LogstashFormatterVersion1
 class LogstashFormatter(LogstashFormatterVersion1):
     def format(self, record, sent_request=None):
         caddr = 'unknown'
+        user = None
+        domain = None
+        useragent = None
         if hasattr(record, 'request'):
             caddr = record.request.META.get('HTTP_X_FORWARDED_FOR') or record.request.META.get('REMOTE_ADDR')
+            user = record.request.user if hasattr(record.request, 'user') else None
+            domain = record.request.META.get('HTTP_HOST')
+            useragent = record.request.META.get('HTTP_USER_AGENT')
+
+        if user:
+            user_id = record.request.user.id
+            is_anonymus = record.request.user.is_anonymous
+        else:
+            user_id = None
+            is_anonymus = True
 
         message = {
             '@timestamp': self.format_timestamp(record.created),
@@ -14,7 +27,12 @@ class LogstashFormatter(LogstashFormatterVersion1):
             'host': self.host,
 
             'client': caddr,
-            'username': str(record.request.user) if hasattr(record, 'request') else None,
+            'geoip.ip': caddr,
+            'user.username': str(user),
+            'user.id': str(user_id),
+            'user.is_anonymus': str(is_anonymus),
+            'domain': domain,
+            'user-agent': str(useragent),
 
             'path': record.pathname,
             'tags': self.tags,
